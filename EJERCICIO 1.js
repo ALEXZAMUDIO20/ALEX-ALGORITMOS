f91c46ba25c76f8d44bd95176a84054124e271ea
ALEXZAMUDIO20 <89936145+ALEXZAMUDIO20@users.noreply.github.com>
2021-09-27 11:14:03 -0500

    Add files via upload

---
+++ EJERCICIO 1.js
@@ -0,0 +1,40 @@
+var Dinero = "5";
+if (Dinero < 10) {
+    console.log("comprar un mazapan");
+} else {
+    (Dinero > 10)
+    console.log("Comprar 2 mazapanes");
+}
+
+var edad = "18";
+if (edad == 18) {
+    console.log("Hola tengo 18");
+} else {
+    (edad < 10)
+    console.log("Hola aun no tengo 18");
+}
+
+var perro = "firulais";
+if (perro == "Jack") {
+    console.log("Acaricio al perro");
+} else {
+    (perro == "firulais")
+    console.log("Bañar al perro");
+}
+
+
+var futbol = "balon";
+if (futbol == "cancha") {
+    console.log("Vamos a jugar");
+} else {
+    (futbol == "balon")
+    console.log("Vamos a bailar");
+}
+
+var pesos = "15";
+if (pesos < 10) {
+    console.log("Compra una coca");
+} else {
+    (pesos > 10)
+    console.log("Compra dulces");
+}
---
+++ main.js
@@ -0,0 +1,21 @@
+/*  
+< menor que
+> mayor que 
+= asignar valor
+== igual en comparacion
+!= diferente
+<= menor o igual
+>= mayor o igual
+&& Y
+// o 
+! negacion
+*/
+
+var x = 300; //variable para comparar
+var y = 20;
+var s = 30;
+if (x < y) {
+    console.log(x + "es menor a" + y);
+} else {
+    console.log(x + "es mayor a" + y)
+}
