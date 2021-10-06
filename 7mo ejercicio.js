6af85368c159cfb6f2410976be73c6bb9ad89431
ALEXZAMUDIO20 <89936145+ALEXZAMUDIO20@users.noreply.github.com>
2021-10-06 11:30:41 -0500

    Delete 7mo ejercicio.js

--- 7mo ejercicio.js
+++
@@ -1,7 +0,0 @@
-console.log("7mo ejercicio");
-
-function contarvocales(texto) {
-    return texto.replace(/[aeiouAEIOUáéíóúÁÉÍÓÚ]/g, "").length;
-}
-Y = prompt("Ingresa cualquier frase...veras la cantidad de vocales que tiene esta")
-console.log(contarvocales(Y));
